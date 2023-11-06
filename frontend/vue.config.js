4b2007056155b447b7c05704e28a6a15a936f6e2
janis <jahu04@outlook.com>
2023-11-06 16:42:38 +0100

    revert vue config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,34 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  pluginOptions: {
-    electronBuilder: {
-      nodeIntegration: true,
-      "appId": "com.janishutz.MusicPlayer",
-      "copyright": "Copyright (c) 2023 MusicPlayer contributors",
-      "buildVersion": "V2.0.0",
-      builderOptions: {
-        files: [
-          "**/*",
-          {
-            from: "./*",
-            to: "./*",
-            filter: [ "**/*" ]
-          }
-        ],
-        extraFiles: [
-          {
-            from: "./src/config",
-            to: "./config",
-            filter: [ "*.config.json" ]
-          },
-          {
-            from: "./src/client",
-            to: "./client",
-            filter: [ "**/*" ]
-          }
-        ]
-      }
-    }
-  }
 })
