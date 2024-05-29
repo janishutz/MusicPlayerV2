eb15e1fc3f0b3ac4874e43de2a73562ab27cc4e4
Janis Hutz <development@janishutz.com>
2024-05-29 08:34:07 +0200

    some initial setup work for GUI

---
+++ MusicPlayerV2-GUI/src/App.vue
@@ -0,0 +1,167 @@
+<!--
+*				libreevent - App.vue
+*
+*	Created by Janis Hutz 05/14/2023, Licensed under the GPL V3 License
+*			https://janishutz.com, development@janishutz.com
+*
+*
+-->
+
+<template>
+    <div>
+        <button @click="changeTheme();" id="themeSelector" title="Toggle between light and dark mode"><span class="material-symbols-outlined" v-html="theme"></span></button>
+        <router-view v-slot="{ Component, route }" id="main-view">
+            <transition :name="route.meta.transition ? String( route.meta.transition ) : 'fade'" mode="out-in">
+                <component :is="Component"></component>
+            </transition>
+        </router-view>
+    </div>
+</template>
+
+<style>
+    body {
+        background-color: var( --background-color );
+    }
+
+    :root, :root.light {
+        --primary-color: #0a1520;
+        --secondary-color: white;
+        --background-color: rgb(221, 221, 221);
+        --nav-background: white;
+        --hover-color: #00457a;
+        --popup-color: rgb(224, 224, 224);
+        --overlay-color: rgba(0, 0, 0, 0.7);
+        --PI: 3.14159265358979;
+        --gray-color: rgb(53, 53, 53);
+        --footer-background: rgb(233, 233, 233);
+        --accent-background: rgb(195, 235, 243);
+        --loading-color: rgb(167, 167, 167);
+    }
+
+    :root.dark {
+        --primary-color: white;
+        --secondary-color: black;
+        --background-color: rgb(32, 32, 32);
+        --nav-background: rgb(54, 54, 54);
+        --popup-color: rgb(58, 58, 58);
+        --hover-color: #007ddd;
+        --overlay-color: rgba(104, 104, 104, 0.575);
+        --gray-color: rgb(207, 207, 207);
+        --footer-background: rgb(53, 53, 53);
+        --accent-background: rgb(24, 12, 58);
+        --loading-color: rgb(65, 65, 65);
+    }
+
+    @media ( prefers-color-scheme: dark ) {
+        :root {
+            --primary-color: white;
+            --secondary-color: black;
+            --background-color: rgb(32, 32, 32);
+            --nav-background: rgb(54, 54, 54);
+            --popup-color: rgb(58, 58, 58);
+            --hover-color: #007ddd;
+            --overlay-color: rgba(104, 104, 104, 0.575);
+            --gray-color: rgb(207, 207, 207);
+            --footer-background: rgb(53, 53, 53);
+            --accent-background: rgb(24, 12, 58);
+            --loading-color: rgb(65, 65, 65);
+        }
+    }
+
+    ::selection {
+        background-color: #7c8cec;
+        color: white;
+    }
+
+    html,
+    body {
+        width: 100%;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+        font-size: 17px;
+    }
+
+    #app {
+        transition: 0.5s;
+        background-color: var( --background-color );
+        font-family: 'Plus Jakarta Sans', sans-serif;
+        /* font-family: Avenir, Helvetica, Arial, sans-serif; */
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: var( --primary-color );
+        display: flex;
+        flex-direction: column;
+        flex-grow: 1;
+        width: 100vw;
+        margin: 0;
+    }
+
+    #main-view {
+        min-height: 60vh;
+    }
+
+    .scale-enter-active,
+    .scale-leave-active {
+        transition: all 0.5s ease;
+    }
+
+    .scale-enter-from,
+    .scale-leave-to {
+        opacity: 0;
+        transform: scale(0.9);
+    }
+
+    .fade-enter-active,
+    .fade-leave-active {
+        transition: opacity 0.4s ease;
+    }
+
+    .fade-enter-from,
+    .fade-leave-to {
+        opacity: 0;
+    }
+
+    .material-symbols-outlined {
+        font-variation-settings:
+        'FILL' 0,
+        'wght' 400,
+        'GRAD' 0,
+        'opsz' 48
+    }
+
+    .clr-open {
+        border: black solid 1px !important;
+    }
+</style>
+
+<script setup lang="ts">
+    import { ref } from 'vue';
+    import { RouterView } from 'vue-router';
+
+    const theme = ref( 'light_mode' );
+
+    const changeTheme = () => {
+        if ( theme.value === 'dark_mode' ) {
+            document.documentElement.classList.remove( 'dark' );
+            document.documentElement.classList.add( 'light' );
+            localStorage.setItem( 'theme', 'light_mode' );
+            theme.value = 'light_mode';
+        } else if ( theme.value === 'light_mode' ) {
+            document.documentElement.classList.remove( 'light' );
+            document.documentElement.classList.add( 'dark' );
+            localStorage.setItem( 'theme', 'dark_mode' );
+            theme.value = 'dark_mode';
+        }
+    }
+
+    theme.value = localStorage.getItem( 'theme' ) ?? '';
+    if ( window.matchMedia( '(prefers-color-scheme: dark)' ).matches || theme.value === 'dark_mode' ) {
+        document.documentElement.classList.add( 'dark' );
+        theme.value = 'dark_mode';
+    } else {
+        document.documentElement.classList.add( 'light' );
+        theme.value = 'light_mode';
+    }
+</script>
---
+++ MusicPlayerV2-GUI/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Hello World</h1>
+        <div class="player-panel-link"></div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
