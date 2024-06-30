c3bff192bb8f6787d6b12ddc82cff9a1c6ebda51
Janis Hutz <development@janishutz.com>
2024-06-30 12:10:57 +0200

    some small updates

--- MusicPlayerV2-GUI/src/views/GetView.vue
+++ MusicPlayerV2-GUI/src/views/GetView.vue
@@ -1,7 +1,27 @@
 <template>
     <div>
-        <h1>Get MusicPlayer</h1>
-        <p>MusicPlayer offers</p>
-        <a href="https://store.janishutz.com/product/com.janishutz.MusicPlayer" class="fancy-button">Subscribe</a>
+        <div class="top-view">
+            <img src="https://github.com/simplePCBuilding/MusicPlayerV2/raw/master/assets/logo.png" alt="MusicPlayer Logo" class="logo">
+            <h1>Get MusicPlayer</h1>
+            <p>MusicPlayer is a Open Source, Web-Based Music Player, which you can use to have other people join your playlist and see live playback status.</p>
+            <p>On music.janishutz.com, MusicPlayer is all set up for you, no need to spend time setting it up yourself</p>
+            <a href="https://store.janishutz.com/product/com.janishutz.MusicPlayer" class="fancy-button">Subscribe</a>
+        </div>
     </div>
 </template>
+
+<style scoped>
+    .logo {
+        height: 40%;
+        border-radius: 20px;
+    }
+
+    .top-view {
+        height: 100vh;
+        width: 100vw;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column; 
+    }
+</style>
